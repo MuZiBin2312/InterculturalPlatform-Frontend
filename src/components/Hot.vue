d16c935ea2a7d2a4ff89d9067b4dfd602638c9ab
ricardoyang7 <RicardoYang7@gmail.com>
2025-05-11 17:49:24 +0800

    Changes

--- src/components/Hot.vue
+++ src/components/Hot.vue
@@ -4,12 +4,12 @@
       <div style="display: flex; align-items: center; margin-bottom: 20px">
         <img src="@/assets/imgs/hot.png" alt="" style="width: 70px">
         <div style="flex: 1; text-align: right; ">
-          <span @click="loadHot" style="font-size: 18px; cursor: pointer"><i class="el-icon-refresh"></i> 换一换</span>
+          <span @click="loadHot" style="font-size: 0.7vw; cursor: pointer"><i class="el-icon-refresh"></i> 换一换</span>
         </div>
       </div>
       <div>
         <div  @click="$router.push('/front/newsDetail?id=' + item.id)" v-for="(item, index) in hotNewsList" :key="item.id"
-              style="font-size: 16px; margin-bottom: 10px; cursor: pointer; color: #666" class="line1">
+              style="font-size: 0.6vw; margin-bottom: 10px; cursor: pointer; color: #666" class="line1">
           <strong style="color: red; margin-right: 10px" v-if="index < 3">{{ index + 1 }}</strong>
           <strong style="color: #333; margin-right: 10px" v-else>{{ index + 1 }}</strong>
           <span>{{ item.title }}</span>
